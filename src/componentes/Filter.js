49b2acb5c61c3d547c0129b23e662c41a7f50b55
MaxRanholi <73243210+MaxRanholi@users.noreply.github.com>
2022-05-10 18:49:25 -0300

    Header

---
+++ src/componentes/Filter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+const ContainerFilter = styled.div`
+color: yellowgreen;
+height: 500px;
+width: 500x;
+`
+
+
+const Filter = () => {
+  return (
+    <ContainerFilter>filter</ContainerFilter>
+  )
+}
+
+export default Filter
